f8ab9574d85605a2d2dbaee1f26acdbfdd5a2ca0
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2021-11-20 11:36:00 +0800

    read config.json to combobox

--- Launcher-main.py
+++ Launcher-main.py
@@ -4,6 +4,7 @@ import os
 from os.path import expanduser
 from PySide2 import QtWidgets, QtCore, QtGui
 import pyautogui
+import numpy as np
 
 def CheckWorkDir():
     HomeDir = expanduser("~")
@@ -38,10 +39,46 @@ if not os.path.isfile("config.json"):
     screen = QtWidgets.QDesktopWidget().screenGeometry()
     '''
     with open("config.json", "w") as f:
-        json.dump({"Fullscreen Resolution": "{screensize}", "Music": "On", "Windowed" : "True"}, f)
-#read json file
-with open("config.json") as json_file:
-    config = json.load(json_file)
+        #add resolution, music, windowed to json
+        json.dump({"resolution": [screensize, "1280 x 720"], "music": "on", "windowed": "on"}, f)
+
+#try json file is readable
+try:
+    with open("config.json", "r") as f:
+        config = json.load(f)
+        '''
+        try:
+            if config["screensize"] == screensize:
+                pass
+        except:
+            print("[WARN] config.json corrupt.")
+            with open("config.json", "w") as f:
+                json.dump({"resolution": screensize, "music": "on", "windowed": "on"}, f)
+        '''
+
+        #print(f"[INFO] Your screen size is {config["resolution"]})
+
+except:
+    print("[WARN] config.json corrupt.")
+    print("[WARN] retry creating new config.json.")
+    #read screen biggest resolution
+    width, height= pyautogui.size()
+    screensize = (f"{width} x {height}")
+    print(f"[INFO] Your screen size is {screensize}")
+    '''
+    screen = QtWidgets.QDesktopWidget().screenGeometry()
+    '''
+    with open("config.json", "w") as f:
+        #add resolution, music, windowed to json
+        json.dump({"resolution": screensize, "music": "on", "windowed": "on"}, f)
+finally:
+    #convert resolution list to string and remove json characters and seprate each value to new line
+    #config["resolution"] = str(config["resolution"]).replace("[", "").replace("]", "").replace("'", "").replace(",", "\n")
+    #resolution = np.array(config["resolution"].split("\n"))
+    print(f"[INFO] The resolution in config is {config['resolution']}")
+    print(f"[INFO] The music in config is {config['music']}")
+    print(f"[INFO] The windowed in config is {config['windowed']}")
+    print('[INFO] Starting launcher window')
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -50,9 +87,7 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui = Ui_Main_Window()
         self.ui.setupUi(self)
         #add combo box text from config.json
-        '''
-        self.ui.Resolution_Settings.addItem(config["Width"] + "x" + config["Height"])
-        '''
+        self.ui.Resolution_Settings.addItems(config["resolution"])
 
 
 if __name__ == '__main__':
