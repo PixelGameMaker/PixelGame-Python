35abb33f23b0164b0d25d52043595c37f8258e40
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-02-10 19:00:34 +0800

    I'm idiot

--- build.cmd
+++ build.cmd
@@ -1,1 +1,1 @@
-pyinstaller -F .\launcher-rework.py --icon=Logo.ico && pyinstaller -F .\main.py && pyinstaller -F .\ErrorWindow.py && pyinstaller -F .\YouLose.py
+pyinstaller -F .\launcher-rework.py --icon=Logo.ico && pyinstaller -F .\main.py --icon=Logo.ico && pyinstaller -F .\ErrorWindow.py --icon=Logo.ico && pyinstaller -F .\YouLose.py --icon=Logo.ico
